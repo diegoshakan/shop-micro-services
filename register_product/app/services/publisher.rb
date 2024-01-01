653442f42800b6daa2f84d9066816d397b44b20f
Diego Araujo <diego_shakan@hotmail.com>
2024-01-01 17:48:46 -0300

    config test environment and bunny gem

---
+++ register_product/app/services/publisher.rb
@@ -0,0 +1,18 @@
+class Publisher
+  QUEUE = 'hello'
+
+  def self.connection
+    conn = Bunny.new(automatically_recover: false)
+    conn.start
+  end
+
+  def self.channel
+    ch = connection.create_channel
+    ch.queue(QUEUE, durable: true)
+  end
+
+  def self.publish(message)
+    channel.publish(message.to_json, routing_key: channel.name, persistent: true)
+    connection.close
+  end
+end
